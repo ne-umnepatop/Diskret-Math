0160648213d3216d8a2929d535622b2747003f04
ne-umnepatop <113514164+ne-umnepatop@users.noreply.github.com>
2022-11-29 17:02:14 +0300

    Скелет программы
    Будет обрастать функциями

---
+++ Дискретка лабораторная.py
@@ -0,0 +1,41 @@
+from collections import Counter
+def cnt(f):
+    '''Функция возвращает словарь из пар буква:количество букв.
+    На вход подаётся txt файл, например, так:
+    with open('disk.txt.txt') as f:
+        print(cnt(f))'''
+    a = []
+    for str in f.readlines():
+        a+=Counter(str).most_common()
+    letter = []
+    amount = []
+    for x, y in a:
+        if letter.count(x) < 1:
+            letter.append(x)
+            amount.append(y)
+        else:
+            amount[letter.index(x)] += y
+    c=sorted(amount)
+    ltrs=[]
+    for i in range(len(c)):
+        ltrs.append(letter[amount.index(c[i])])
+        letter.pop(amount.index(c[i]))
+        amount.pop(amount.index(c[i]))
+    a = dict(zip(ltrs, c))
+    return a
+
+
+def main():
+    '''Тут основной код программы
+    Нужно:
+    1) Написать функцию подсчёта символов -- Сделано
+    2) создать функцию для шифровки
+    3) Реализовать ввод аргументов из командной строки
+    Вместо 'disk.txt' будет как раз он'''
+    txt='disk.txt'
+    with open(f'{txt}') as f:
+        print(cnt(f))
+
+
+if __name__=='__main__':
+    main()
---
+++ Количество символов.py
@@ -0,0 +1,33 @@
+from collections import Counter
+def cnt(f):
+    '''Функция возвращает словарь из пар буква:количество букв.
+    На вход подаётся txt файл, например, так:
+    with open('disk.txt.txt') as f:
+        print(cnt(f))'''
+    a = []
+    for str in f.readlines():
+        a+=Counter(str).most_common()
+    letter = []
+    amount = []
+    for x, y in a:
+        if letter.count(x) < 1:
+            letter.append(x)
+            amount.append(y)
+        else:
+            amount[letter.index(x)] += y
+    c=sorted(amount)
+    ltrs=[]
+    for i in range(len(c)):
+        ltrs.append(letter[amount.index(c[i])])
+        letter.pop(amount.index(c[i]))
+        amount.pop(amount.index(c[i]))
+    a = dict(zip(ltrs, c))
+    return a
+
+
+txt='disk.txt'
+with open(f'{txt}') as f:
+    print(cnt(f))
+
+
+
