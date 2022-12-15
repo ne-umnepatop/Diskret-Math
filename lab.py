63ab86acf53fdd0f917fc435b0eb0b78f89d9ea2
ne-umnepatop <113514164+ne-umnepatop@users.noreply.github.com>
2022-12-15 20:52:48 +0300

    Добавлены комментарии

--- lab.py
+++ lab.py
@@ -1,89 +1,113 @@
 # -*-coding:cp1251-*-
 import sys
-parlist = []
-for p in sys.argv:
-    parlist.append(p)
-itype = parlist[1]
-input_file = parlist[2]
-out_file = parlist[3]
-if itype == "--encode":
-    f = open(input_file)
-    x = ''
-    for i in f:
-        x += i
-    al = {}
-    for i in x:
-        al[i] = 0
-    fl = len(x)
-    for i in x:
-        al[i] += 1
-    l = len(al)
-    bl = al.copy()
-    for i in bl:
-        bl[i] = ''
-    if len(al) == 1:
-        bl[i] = '0'
-    while len(al) > 1:
-        a = min(al, key=al.get)
-        an = al[a]
-        al.pop(a)
-        b = min(al, key=al.get)
-        bn = al[b]
-        al.pop(b)
-        for i in a:
-            bl[i] = '0' + bl[i]
-        for i in b:
-            bl[i] = '1' + bl[i]
-        al[a + b] = an + bn
-    for i in bl:
-        x = x.replace(i, bl[i])
-    output = open(out_file, 'w')
-    output.write(str(l) + '\n')
-    output.write(str(bl) + '\n')
-    output.write(x + '\n')
-    output.close()
-if itype == "--decode":
-    input_file = open(input_file)
-    holder = ''
-    ll = []
-    for i in input_file:
-        holder += str(i)
-    ll = holder.split('\n')
-    l = int(ll[0])
-    al = ll[1]
-    code = ll[2]
-    jo = {}
-    ja = {}
-    for i in range(l):
-        ja[al.split(', ')[i].split(': ')[1].split("'")[1]] = al.split(', ')[i].split(': ')[0].split("'")[1]
-    out = ''
-    newlist = []
-    notnewlist = []
-    for k in ja:
+def main():
+    # работа с опциями из командной строки
+    parlist = []
+    for p in sys.argv:
+        parlist.append(p)
+    itype = parlist[1]
+    input_file = parlist[2]
+    out_file = parlist[3]
+    # шифровка
+    if itype == "--encode":
+        # составление словаря из пар символ - частота встреч
+        f = open(input_file)
+        x = ''
+        for i in f:
+            x += i
+        al = {}
+        for i in x:
+            al[i] = 0
+        fl = len(x)
+        for i in x:
+            al[i] += 1
+        l = len(al)
+        # получаем словарь из пар символ - пустой символ
+        bl = al.copy()
+        for i in bl:
+            bl[i] = ''
+        # если во всём тексте только один символ, кодируем его 0
+        if len(al) == 1:
+            bl[i] = '0'
+        # пока остались незакодированные символы
+        while len(al) > 1:
+            # а - индекс самого редкого символа
+            a = min(al, key=al.get)
+            # an - самый редкий символ
+            an = al[a]
+            # убираем из al
+            al.pop(a)
+            # то же с б
+            b = min(al, key=al.get)
+            bn = al[b]
+            al.pop(b)
+            # добавить комментарий
+            for i in a:
+                bl[i] = '0' + bl[i]
+            for i in b:
+                bl[i] = '1' + bl[i]
+            # объединили два символа с наименьшей  частотностью встреч
+            al[a + b] = an + bn
+        # непосредственно шифруем
+        for i in bl:
+            x = x.replace(i, bl[i])
+        output = open(out_file, 'w')
+        output.write(str(l) + '\n')
+        output.write(str(bl) + '\n')
+        output.write(x + '\n')
+        output.close()
+    # дешифруем
+    if itype == "--decode":
+        input_file = open(input_file)
+        holder = ''
+        ll = []
+        for i in input_file:
+            holder += str(i)
+        ll = holder.split('\n')
+        # считали кол-во различных символов
+        l = int(ll[0])
+        # считали словарь
+        al = ll[1]
+        code = ll[2]
+        # считали то, что будем расшифровывать
+        ja = {}
+        for i in range(l):
+            # развернули словарь кодами вперёд
+            ja[al.split(', ')[i].split(': ')[1].split("'")[1]] = al.split(', ')[i].split(': ')[0].split("'")[1]
+        out = ''
+        newlist = []
+        notnewlist = []
         try:
-            newlist.append(k)
-            notnewlist.append(int(k))
+            for k in ja:
+                # добавить комментарий
+                newlist.append(k)
+                notnewlist.append(int(k))
+            ma = len(str(max(notnewlist)))
+        # если мы не можем добавить элемент К, его не существует, тогда длина = 0
         except:
             ma = 0
-    try:
-        ma = len(str(max(notnewlist)))
-    except:
-        pass
-    code += ' ' * ma
-    i = 0
-    while ('1' in code or '0' in code) and i < len(code) - ma:
-        if code[i] == '1' or code[i] == '0':
-            ho = ''
-            for j in range(ma):
-                ho += str(code[i + j])
-                if ho in newlist:
-                    if ja[ho] == "\\n":
-                        code = code.replace(ho, '\n', 1)
-                    else:
-                        code = code.replace(ho, ja[ho], 1)
-                        break
-        i += 1
-    out = code[:-ma]
-    out_file = open(out_file, 'w')
-    out_file.write(out)
-    out_file.close()
+        # добавить комментарий
+        code += ' ' * ma
+        i = 0
+        # пока остаолсь неравскодированное
+        while ('1' in code or '0' in code) and i < len(code) - ma:
+            if code[i] == '1' or code[i] == '0':
+                ho = ''
+                # добавить
+                for j in range(ma):
+                    ho += str(code[i + j])
+                    if ho in newlist:
+                        if ja[ho] == "\\n":
+                            code = code.replace(ho, '\n', 1)
+                        else:
+                            code = code.replace(ho, ja[ho], 1)
+                            break
+            i += 1
+        # пишем изменения
+        out = code[:-ma]
+        out_file = open(out_file, 'w')
+        out_file.write(out)
+        out_file.close()
+# так принято в питоне, если мы станем использовать шифратор в другой программе, это очень удобно
+if __name__=='__main__':
+    main()
